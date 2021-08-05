4886569d1ceb577826ae02448fca22ef38f7161d
Sera-Reji <81759303+Sera-Reji@users.noreply.github.com>
2021-08-05 19:03:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var canvas=document.getElementById("myCanvas");
+var ctx=canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle="red";
+ctx.lineWidth=1;
+ctx.rect(150, 143, 430, 200 * Math.PI);
+ctx.stroke();
+ctx.beginPath();
+ctx.strokeStyle="blue";
+ctx.lineWidth=5;
+ctx.arc(150, 143, 40, 0, 2 * Math.PI);
+ctx.stroke();
